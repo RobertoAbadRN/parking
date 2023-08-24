34587243474fc47cb9338ac8b9e1ab727b5705ef
factorycode <martin.amador.tic@gmail.com>
2023-08-24 16:59:29 -0600

    errorees
    errores

--- resources/views/registration.blade.php
+++ resources/views/registration.blade.php
@@ -1,21 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Tus metadatos, enlaces a estilos, etc. -->
-</head>
-<body>
-   <!-- ... (código anterior) -->
+<x-base-layout title="Register" is-sidebar-open="true" is-header-blur="true">
 
-<div class="min-h-screen flex items-center justify-center bg-gray-100">
-    <div class="bg-white p-6 rounded shadow-md">
-        <h1 class="text-2xl font-semibold mb-4">Registration Status</h1>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.all.min.js"></script>
-        auto registado con exito 
-    </div>
-</div>
+    <main class="main-content w-full px-[var(--margin-x)] pb-8">
 
-<!-- ... (código posterior) -->
+        <div class="min-h-screen flex items-center justify-center bg-gray-100">
+            <div class="bg-white p-6 rounded shadow-md">
+                <h1 class="text-2xl font-semibold mb-4">Registration Status</h1>
+                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10.16.6/dist/sweetalert2.all.min.js"></script>
+                @if (session('success'))
+                    <div class="text-green-500 text-lg">{{ session('success') }}</div>
+                @endif
+                @if (session('error'))
+                    <div class="text-red-500 text-lg">
+                        <p>Vehicle Registration Error</p>
+                        <p>Vehicle Registration for Permit at {{ session('property_code') }}</p>
+                        <p>We are sorry but your vehicle could not be registered.</p>
+                        <p>Please visit {{ session('property_code') }} office for further information and resolve this
+                            issue.</p>
+                    </div>
+                @endif
 
-</body>
-</html>
+            </div>
+        </div>
 
+        <!-- ... (later code) -->
+    </main>
+
+</x-base-layout>
